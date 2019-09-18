e1e601e7b86f3be38b3c2ac2046e8ffebd76043e
shashanks01 <shash0124@gmail.com>
2019-09-18 18:38:09 +0530

    ENH: config file, refractoring

--- yasmss/mrresult/groupby_result.py
+++ yasmss/mrresult/groupby_result.py
@@ -3,13 +3,27 @@
 """
 import subprocess
 import os
+import yaml
 
 class MrResult:
     def __init__(self,queryset):
-        self.output = "hdfs://localhost:9000/test/output_gx2/part-00000"
         self.json_result = []
         self.col_names = []
         self.queryset = queryset
+        self.output = None
+        self.opfile = "part-00000"
+        self.outputpath = None
+        self.outputdir = None
+
+    def _get_outputpath(self):
+        conf = {}
+        with open("config.yaml", 'r') as file:
+            conf = yaml.load(file, Loader=yaml.FullLoader)
+        self.outputpath = conf['output']
+        self.outputdir = conf['outputdir']
+        return "hdfs://localhost:9000/{path}/{outputdir}/{outputfile}".format(
+                path=self.outputpath, outputdir=self.outputdir, outputfile=self.opfile)
+
 
     def _get_col_names(self):
         selcols = self.queryset.selcolumns[:len(self.queryset.selcolumns)-1]
@@ -17,15 +31,20 @@ class MrResult:
         return selcols
 
     def _groupby_res(self):
+        """
+        mapping the column names to the output stored in hdfs
+        """
         self.col_names = self._get_col_names()
+        self.output = self._get_outputpath()
         cat = subprocess.Popen(["hadoop", "fs", "-cat", self.output], stdout=subprocess.PIPE)
         for line in cat.stdout:
-            self.json_result.append(dict(zip(self.col_names, line.decode('utf-8').split('\n')[0].replace(",", "\t").split('\t'))))
-            # t = dict(zip(self.[col_names], line.decode('utf-8').split('\n')[0].split('\t')))
+            temp_dict = dict(zip(self.col_names, line.decode('utf-8').split('\n')[0].replace(",", "\t").split('\t')))
+            self.json_result.append(temp_dict)
         self._cleanup()
 
     def _cleanup(self):
-        os.system("hdfs dfs -rm -r hdfs://localhost:9000/test/output_gx2")
+        rm_output = "hdfs dfs -rm -r hdfs://localhost:9000/{path}/{outputdir}".format(path=self.outputpath, outputdir=self.outputdir)
+        os.system(rm_output)
 
     def get_result(self):
         self._groupby_res()
